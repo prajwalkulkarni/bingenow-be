8cb8ecfb52da002b14eea4ddf1ea5e42226bf3c9
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2023-04-01 00:22:20 +0530

    Set response type

--- index.ts
+++ index.ts
@@ -191,7 +191,8 @@ app.use((req: Request, res: Response, next: Function) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization')
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,PUT,DELETE,OPTIONS')
-
+    res.set('Content-Type', 'application/json');
+    
     next()
 })
 
